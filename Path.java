a345263d393611fca1d24eb1e7f4ae8990b5685e
Caua-Festa <100861169+CSFesta@users.noreply.github.com>
2023-11-09 20:29:35 -0300

    Update Path.java

--- Path.java
+++ Path.java
@@ -18,16 +18,14 @@ public class Path {
 		ArrayList<No> graph = new ArrayList<>();
 
 		for (int i = 0; i < n; i++) {
-			No no = new No(new ArrayList<Integer>(), i);
-			graph.add(no);
+			graph.add(new No(new ArrayList<Integer>(), i));
 		}
 		for (int i = 0; i < edges.length; i++) {
 			graph.get(edges[i][0]).adj().add(edges[i][1]);
 			graph.get(edges[i][1]).adj().add(edges[i][0]);
 		}
 		boolean[] visited = new boolean[n];
-		boolean x = dfs(visited, source, destination, false, graph);
-		return x;
+		return = dfs(visited, source, destination, false, graph);
 	}
 
 	private static boolean dfs(boolean[] visited, int u, int destination, boolean res, ArrayList<No> graph) {
