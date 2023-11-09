3746f42cf843356429d46c74bbd4b918b1957dd7
CSFesta <cauafesta@hotmail.com>
2023-11-09 20:19:43 -0300

    Correcao do Programa

--- Path.java
+++ Path.java
@@ -5,42 +5,41 @@ import java.util.ArrayList;
 public class Path {
 
 	public static record No(ArrayList<Integer> adj, int V) {
-		public void CreateAdj(int v) {
-			adj.add(v);
-		}
 	}
-
-	private static ArrayList<No> graph = new ArrayList<>();
 
 	public static void main(String[] args) {
 
+		System.out.println(validPath(6, new int[][] { { 0, 1 }, { 2, 4 }, { 4, 5 }, { 1, 5 }, { 0, 3 } }, 0, 2));
 		System.out.println(validPath(3, new int[][] { { 0, 1 }, { 1, 2 }, { 2, 0 } }, 0, 2));
 		System.out.println(validPath(6, new int[][] { { 0, 1 }, { 0, 2 }, { 3, 5 }, { 5, 4 }, { 4, 3 } }, 0, 5));
 	}
 
 	public static boolean validPath(int n, int[][] edges, int source, int destination) {
+		ArrayList<No> graph = new ArrayList<>();
+
 		for (int i = 0; i < n; i++) {
 			No no = new No(new ArrayList<Integer>(), i);
 			graph.add(no);
 		}
 		for (int i = 0; i < edges.length; i++) {
-			graph.get(edges[i][0]).CreateAdj(edges[i][1]);
-			graph.get(edges[i][1]).CreateAdj(edges[i][0]);
+			graph.get(edges[i][0]).adj().add(edges[i][1]);
+			graph.get(edges[i][1]).adj().add(edges[i][0]);
 		}
 		boolean[] visited = new boolean[n];
-		return dfs(visited, source, destination);
+		boolean x = dfs(visited, source, destination, false, graph);
+		return x;
 	}
 
-	private static boolean dfs(boolean[] visited, int u, int destination) {
+	private static boolean dfs(boolean[] visited, int u, int destination, boolean res, ArrayList<No> graph) {
 		visited[u] = true;
-		if (!graph.get(u).adj.contains(destination)) {
-			for (int i = 0; i < visited.length; i++) {
-				if (!visited[i] && graph.get(u).adj.contains(i)) {
-					dfs(visited, i, destination);
-				}
-			}
-			return false;
+		if (graph.get(u).adj.contains(destination)) {
+			return true;
 		}
-		return true;
+		for (int i = 0; i < visited.length; i++) {
+			if (!visited[i] && graph.get(u).adj.contains(i)) {
+				res = dfs(visited, i, destination, res, graph);
+			}
+		}
+		return res;
 	}
 }
